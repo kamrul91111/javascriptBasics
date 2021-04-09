923ab4269b1686846a5a98d53faffbe595c71536
kamrul91111 <13157019@student.uts.edu.au>
2021-04-09 16:27:13 +1000

    js course from mosh

---
+++ index.js
@@ -0,0 +1,21 @@
+//javascript is all about behavior
+//all statements in js has to be terminated with a (;)
+//naming conventions, they cannot be reserved keywords such as let, if, else, var, const etc. 
+//should be meaningful
+//cannot start with a number
+//cannot contain multiple words, nor hyphens. Just use camelCase
+//definitely case sensitive
+
+//const means constant, as in it will remain the same instead of declaring it as let
+// const interestRate = 0.3;
+// console.log(interestRate);
+
+//they are all variables, hence declared with let instead of const
+let name = 'Kamrul'; //String Literal
+let age = 23; // Number Literal
+let isApproved = true; // boolean Literal
+let firstName = undefined; // undefined
+let selectedColor = null; //null
+
+console.log(name);
+
